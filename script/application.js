4d5d20944d20efaf186a447ef6162639be45d5e2
oe22an <oe22an@student.lnu.se>
2010-03-19 15:44:51 +0100

    Testar att commita

--- script/application.js
+++ script/application.js
@@ -1,13 +1,45 @@
 (function($){
-	var appname = 'Application',
+	var appname = 'Storyteller',
 	Singleton = {
 		init: function(){
 			$("h1").text("App initialized!");
 		},
 		data: {
-			somedata: 'foobar',
+			somedata: 'foobar'
 		}
 	};
+	
 	window[appname] = Singleton;  // expose the singleton
-	Singleton.init();                   // initiate application
+	Singleton.init();             // initiate application
 })(jQuery);
+
+var Story = Class.extend({
+	
+	init: function(){
+		var storyName;
+		var storyPartArray;
+	},
+	
+	showStory: function() {},
+	showLastPart: function() {},
+	addStoryPart: function() {}
+});
+
+var StoryPart = Class.extend({
+	
+	init: function(){
+		var storyPartId;
+		var userId;
+		var storyLine;
+	}
+});
+
+var User = Class.extend({
+	
+	init: function(){
+		var userId;
+		var firstName;
+		var lastName;
+		var email;
+	}
+});
